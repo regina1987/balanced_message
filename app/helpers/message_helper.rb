0fdb8c6537a8e623562e847d487730ef84f6ef9d
regiwa <mailofregina@gmail.com>
2021-12-07 23:46:26 -0300

    fix

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -1,15 +1,14 @@
 module MessageHelper
   def self.check_message(s)
     result = true
-    if s.count('(') != s.count(')')
-      s.gsub!(':)', '')
-      s.gsub!(':(', '')
-    end
+    s = clear_from_smiles(s)
     while result == true && s.length.positive?
       pre_open = s.reverse =~ /[(]/ # buscamos ultimo (
       open = !pre_open.nil? ? s.length - 1 - pre_open : nil
-      close = s =~ /[)]/ # buscamos primero )
-      if !close.nil? && !open.nil? && (open < close)
+
+      # buscamos ) que esta mas cerca para (
+      close = !open.nil? ? find_first_close(open, s) : nil
+      if !open.nil? && !close.nil?
         snew = s[open + 1..close - 1]
         snew.gsub!(':)', '')
         snew.gsub!(':(', '')
@@ -31,5 +30,24 @@ module MessageHelper
       result = s.length.positive? ? false : true
     end
     result
+  end
+
+  def self.clear_from_smiles(line)
+    has_variant = true
+    while (line.count('(') != line.count(')')) && line.length.positive? && has_variant
+      line.count('(') > line.count(')') ? line.sub!(':(', '') : line.sub!(':)', '')
+      has_variant = false unless line.include?(':(' || ':)')
+    end
+    line
+  end
+
+  def self.find_first_close(open, line)
+    close = nil
+    j = open + 1
+    while j <= line.length && close.nil?
+      close = j if line[j] == ')'
+      j += 1
+    end
+    close
   end
 end
